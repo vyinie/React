2b66ec0ce4fbe93c00ce61f5879fd486610f7550
Marcus Vinicius <110700931+vyinie@users.noreply.github.com>
2023-04-27 15:44:16 -0300

    Add files via upload

--- jogo das cores/src/jsx/App.jsx
+++ jogo das cores/src/jsx/App.jsx
@@ -8,36 +8,48 @@ function App() {
   const [trueColor, setTrueColor] = useState(colorRandom());
   const [lifes, setLifes] = useState(3);
   const [score, setScore] = useState(0);
-  const [hightScore, setHightScore] = useState(0);
+  useEffect(() => {
+    localStorage.getItem("hightScore") == null &&
+      localStorage.setItem("hightScore", 0);
+  }, []);
+  const [hightScore, setHightScore] = useState(
+    localStorage.getItem("hightScore")
+  );
 
   //========================= limita a vida =======================================
   useEffect(() => {
-    if (lifes == 0) {
-      document.querySelector(".popupWrapper").style.display = "flex";
+    if (lifes === 0) {
+      setTimeout(() => {
+        document.querySelector(".popupWrapper").style.display = "flex";
+      }, 1000);
     }
   }, [lifes]);
 
   //========================= registra os pontos =======================================
   useEffect(() => {
     if (score > hightScore) {
-      setHightScore(score);
+      localStorage.setItem("hightScore", score);
+      setHightScore(localStorage.getItem("hightScore"));
     }
   }, [score]);
 
-  // ========================= score area =======================================
+  const [openDifOn, setOpenDifOn] = useState(false);
+
+  function openDifChecker(e) {
+    if (e.target.className != "difficultyControl") {
+      setOpenDifOn(!openDifOn);
+      openDifOn
+        ? (document.querySelector(".difFControlWrapper").style.display = "flex")
+        : (document.querySelector(".difFControlWrapper").style.display = "");
+    }
+  }
 
   return (
     <div className="App">
-      {/* ========================= score area ======================================= */}
-      <div className="scoreContainer">
-        <div className="score">
-          <h2>Pontos</h2>
-          <h2 id="currentlyScore">{score}</h2>
-        </div>
-        <div className="score">
-          <h2>Record</h2>
-          <h2 id="hightScore">{hightScore}</h2>
-        </div>
+      <div onClick={(e) => openDifChecker(e)} className="oppenDifOpts">
+        <div className="lineOpenDif" id="line1"></div>
+        <div className="lineOpenDif" id="line2"></div>
+        <div className="lineOpenDif" id="line3"></div>
       </div>
 
       {/* ========================= main layout ======================================= */}
@@ -54,25 +66,26 @@ function App() {
         setTrueColor={setTrueColor}
         score={score}
         setScore={setScore}
+        openDifChecker={openDifChecker}
       />
+
+      {/* ========================= score area ======================================= */}
+      <div className="scoreContainer">
+        <div className="score">
+          <h2>Pontos</h2>
+          <h2 id="currentlyScore">{score}</h2>
+        </div>
+        <div className="score">
+          <h2>Record</h2>
+          <h2 id="hightScore">{hightScore}</h2>
+        </div>
+      </div>
 
       {/* ========================= popup to loosers ======================================= */}
       <div className="popupWrapper">
         <div className="popup">
           <h2>perdeu, troxa</h2>
-          <button
-            className="popupBtn"
-            onClick={() => {
-              document.querySelector(".popupWrapper").style.display = "";
-              document.getElementById("btnSetLvlPleno").style
-                .backgroundColor !== ""
-                ? setLifes(1)
-                : setLifes(3);
-              setScore(0);
-            }}
-          >
-            Recomeçar
-          </button>
+          <button className="popupBtn">Recomeçar</button>
         </div>
       </div>
     </div>
