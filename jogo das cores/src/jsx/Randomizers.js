a524e56ce2ca1cfead60957bf11615bd2db9d11f
Marcus Vinicius <110700931+vyinie@users.noreply.github.com>
2023-04-28 17:42:12 -0300

    Delete jogo das cores directory

--- jogo das cores/src/jsx/Randomizers.js
+++
@@ -1,8 +0,0 @@
-export const idRandom = (num) => Math.floor(Math.random() * num);
-
-export const colorRandom = () => {
-  const r = Math.floor(Math.random() * 255);
-  const g = Math.floor(Math.random() * 255);
-  const b = Math.floor(Math.random() * 255);
-  return `rgb(${r}, ${g}, ${b})`;
-};
